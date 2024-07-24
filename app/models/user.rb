056560a56996253ce6cd636c8a34d2cb8698dc78
ttu-wtag <tahsin.turab@welldev.io>
2024-07-24 14:06:38 +0600

    user can update customized field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   enum role: [:admin, :recruiter, :candidate]
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
