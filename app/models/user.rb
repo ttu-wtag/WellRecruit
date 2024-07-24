7447ed5bb356a8f7aacdcea09e82123d47becb26
ttu-wtag <tahsin.turab@welldev.io>
2024-07-24 13:53:32 +0600

    install devise

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: [:admin, :recruiter, :candidate]
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :role, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+end
