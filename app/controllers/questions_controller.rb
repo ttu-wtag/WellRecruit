bd484a68ee8d9b3afecc1a12330750be005ab58a
ttu-wtag <tahsin.turab@welldev.io>
2024-07-25 15:09:32 +0600

    change the view of question's index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -27,7 +27,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to question_url(@question), notice: "Question was successfully created." }
+        format.html { redirect_to assessment_questions_path(@assessment), notice: "Question was successfully created." }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -51,10 +51,11 @@ class QuestionsController < ApplicationController
 
   # DELETE /questions/1 or /questions/1.json
   def destroy
+    @assessment = @question.assessment
     @question.destroy!
 
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: "Question was successfully destroyed." }
+      format.html { redirect_to assessment_questions_path(@assessment), notice: "Question was successfully destroyed." }
       format.json { head :no_content }
     end
   end
