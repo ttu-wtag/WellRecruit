092d18d0068e78ccaff4affb0337e27e09cb6101
ttu-wtag <tahsin.turab@welldev.io>
2024-07-29 10:40:11 +0600

    fixed company creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :name, :company])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :name, :company_id])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :address, :avatar])
   end
 end
