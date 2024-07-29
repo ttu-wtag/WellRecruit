d349a9981f1fcb70a18ba3b1780e23a9a3dcacf1
ttu-wtag <tahsin.turab@welldev.io>
2024-07-29 11:37:02 +0600

    Approve admin by recruiter

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,29 @@
+class AdminController < ApplicationController
+  before_action :set_recruiter, only: [:approve_recruiter, :destroy_recruiter]
+
+  def index
+    @recruiters = User.where(role: :recruiter, company_id: current_user.company.id)
+  end
+
+  def approve_recruiter
+    if @recruiter.update(is_verified: true)
+      flash[:notice] = "Recruiter '#{@recruiter.name}' has been verified."
+    else
+      flash[:notice] = "There arise some issues!"
+    end
+    redirect_to admin_index_path
+  end
+
+  def destroy_recruiter
+    @recruiter.destroy!
+    flash[:notice] = "Recruiter has been deleted."
+    redirect_to admin_index_path
+  end
+
+  private
+
+  def set_recruiter
+    @recruiter = User.find_by_id(params[:recruiter_id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  get 'approve_recruiter/:recruiter_id', to: 'admin#approve_recruiter', as: 'approve_recruiter'
+  get 'destroy_recruiter/:recruiter_id', to: 'admin#destroy_recruiter', as: 'destroy_recruiter'
   resources :companies
   get 'my_company', to: 'companies#my_company', as: :my_company
 
