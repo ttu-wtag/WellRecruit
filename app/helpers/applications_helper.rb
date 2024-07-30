d4372be33dc41efbd194041f727bd02c02bed711
ttu-wtag <tahsin.turab@welldev.io>
2024-07-30 15:55:53 +0600

    assessment details added to the candidate's application after selec

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -2,4 +2,22 @@ module ApplicationsHelper
   def participation_score(application)
     application.participation.present? ? application.participation.score : nil
   end
+
+  def assessment(application)
+    application.selected? and application.job.assessment.present? ? application.job.assessment : nil
+  end
+
+  def schedule(application)
+    assessment = assessment(application)
+    utc_time = Time.parse(assessment.starting_time.to_s).utc
+    time_zone = 'Asia/Dhaka'
+
+    local_time = utc_time.in_time_zone(time_zone)
+    local_time.strftime("%A, %B %d, %Y %I:%M %p %Z")
+  end
+
+  def duration(application)
+    assessment = assessment(application)
+    (Time.parse(assessment.ending_time.to_s) - Time.parse(assessment.starting_time.to_s)) / 60
+  end
 end
