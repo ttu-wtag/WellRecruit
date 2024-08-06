d75aaa7a5c0ea5c990e47b08185e7efcb58c2b75
ttu-wtag <tahsin.turab@welldev.io>
2024-08-06 15:00:03 +0600

    changes schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_30_104151) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_085146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_30_104151) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_jobs_on_company_id"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
@@ -151,6 +153,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_30_104151) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -162,6 +166,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_30_104151) do
   add_foreign_key "applications", "users"
   add_foreign_key "assessments", "jobs"
   add_foreign_key "companies", "users"
+  add_foreign_key "jobs", "companies"
   add_foreign_key "jobs", "users"
   add_foreign_key "participations", "applications"
   add_foreign_key "participations", "assessments"
